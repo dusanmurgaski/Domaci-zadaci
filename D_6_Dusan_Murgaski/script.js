bac8ce0febede58e477ab3956c1b2f9002d652d2
dusanmurgaski <dushan_murgaski@hotmail.com>
2021-04-21 15:39:48 +0200

    domacibr6

--- D_6_Dusan_Murgaski/script.js
+++ D_6_Dusan_Murgaski/script.js
@@ -1,1 +1,29 @@
-console.log("jendavasd");
+// Prvi zadatak
+let f = 200;
+let c = ((f - 32) * 5 / 9);
+console.log(c);
+let kelvin = c + 273.15;
+console.log(kelvin);
+
+
+// Drugi zadatak 
+
+let n = 15;
+let a = 5;
+let ostatakPoglavnja = (n - a - (a + 2));
+console.log(ostatakPoglavnja);
+
+// Treci zadatak
+
+let p = 500;
+let m = 600;
+let k = 300;
+
+let novac = (p + m);
+let cenaDvaDzempera = (novac - k);
+let cenaJednogDzempera = (cenaDvaDzempera / 2);
+console.log(cenaJednogDzempera);
+let pk = (500 - cenaJednogDzempera);
+let mk = (600 - cenaJednogDzempera);
+console.log(pk);
+console.log(mk);
