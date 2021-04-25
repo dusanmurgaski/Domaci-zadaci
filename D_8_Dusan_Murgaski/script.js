461652306ec11f1582d68b147f4f90ceec631968
dusanmurgaski <dushan_murgaski@hotmail.com>
2021-04-25 17:03:03 +0200

    Domaci_Br_8

---
+++ D_8_Dusan_Murgaski/script.js
@@ -0,0 +1,48 @@
+// Prvi zadatak
+
+let radnovremeod = 9;
+let radnovremedo = 20;
+let radnovremeVod = 10;
+let radnovremeVdo = 18;
+
+let m = new Date();
+let dan = m.getDay();
+let sat = m.getHours();
+
+if (dan >= 6, dan == 0) {
+    if (sat >= 10) {
+        if (sat < 18) {
+            console.log("radi");
+        }
+        else {
+            console.log("ne radi")
+        }
+
+    }
+}
+else if (dan > 0, dan < 6) {
+    if (sat >= 9) {
+        if (sat < 20) {
+            console.log("Radi");
+        }
+        else {
+            console.log("ne radi")
+        }
+    }
+}
+
+// Drugi zadatak
+
+let brStanovnika = 1500000;
+let brTest = 10000;
+let brPoz = 1000;
+
+
+if (brPoz >= 0.3 * brTest) {
+    if (brPoz >= 0.1 * brStanovnika) {
+        document.body.innerHTML = `<h1 style="color:red"> vanredno stanje </h1>`;
+    }
+}
+
+
+
